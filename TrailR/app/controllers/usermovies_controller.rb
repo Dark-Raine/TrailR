c57031bee0d9ba53d7deb634ba06e9d738bfaacf
Dark-Raine <anderson-tomas@hotmail.com>
2019-03-05 16:16:32 +0000

    login/create user complete

---
+++ TrailR/app/controllers/usermovies_controller.rb
@@ -0,0 +1,2 @@
+class UsermoviesController < ApplicationController
+end
--- TrailR/config/routes.rb
+++ TrailR/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :usermovies
+  resources :movies
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  post 'api/v1/signin', to: 'api/v1/users#signin'
+  post 'api/v1/register', to: 'api/v1/users#register'
+  get 'api/v1/validate', to: 'api/v1/users#validate'
+  post 'api/v1/collection', to: 'api/v1/users#get_collection'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
