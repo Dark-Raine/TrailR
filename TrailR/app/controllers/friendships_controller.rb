0fa3bb3db2b746b9df412036b1bcaa7bd0f99bd6
github username <github email address>
2019-03-06 12:11:13 +0000

    added friendship models

--- TrailR/app/controllers/friendships_controller.rb
+++ TrailR/app/controllers/friendships_controller.rb
@@ -1,2 +1,17 @@
 class FriendshipsController < ApplicationController
+    def create
+        # byebug
+        @mates = Friendship.create(user_id:params[:user_id], friend_id:params[:friend_id])
+
+        @mates_again = Friendship.create(user_id:params[:friend_id], friend_id:params[:user_id])
+
+        byebug
+        
+      end
+    
+    #   private
+    
+    #   def mess_params(*args)
+    #     params.require(:message).permit(args)
+    #   end
 end
--- TrailR/db/migrate/20190306094938_create_friendships.rb
+++ TrailR/db/migrate/20190306094938_create_friendships.rb
@@ -1,9 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.string :user1_id
-      t.string :user2_id
-
+      t.belongs_to :user
+      t.belongs_to :friend, class: 'User'
       t.timestamps
     end
   end
