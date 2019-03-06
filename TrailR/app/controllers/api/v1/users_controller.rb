61060e84c15260a78504ac848f337e49ae2c766c
Dark-Raine <anderson-tomas@hotmail.com>
2019-03-06 13:59:03 +0000

    corrected validation request

--- TrailR/app/controllers/api/v1/users_controller.rb
+++ TrailR/app/controllers/api/v1/users_controller.rb
@@ -25,14 +25,14 @@ class Api::V1::UsersController < ApplicationController
 
     def validate
         @user = get_current_user
-        if @user && @user.authenticate(params[:password])
+        if @user 
             render json: {username: @user.username, token: issue_token({id: @user.id})}
         else
             render json: {error: "username/password combination invalid"}, status: 401
         end
     end
     
-    def collection
+    def get_collection
         @user = get_current_user
         if @user
             render json: @user.movies
