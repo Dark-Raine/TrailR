6670d5af5075d04edaceacfdea53b2a207f0aa02
github username <github email address>
2019-03-07 11:27:46 +0000

    added user render

--- TrailR/app/controllers/users_controller.rb
+++ TrailR/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all 
+        render json: @users 
+    end 
+
     def my_movies 
         @my_movies = User.find(params[:id]).movies 
         render json: @my_movies
